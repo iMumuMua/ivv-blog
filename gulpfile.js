634b70b3f9895772c661ddd967da3a8bf4b7ee83
iMumuMua <imumu@foxmail.com>
2016-01-04 20:47:12 +0800

    完成主页

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,3 @@
-// node module
-var fs = require('fs');
-var path = require('path');
-
 // gulp module
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
@@ -12,47 +8,13 @@ var autoprefixer = require('autoprefixer');
 var mqpacker = require('css-mqpacker');
 var csswring = require('csswring');
 
-// html process
-var jade = require('jade');
-
-// article data
-var articles = require('./src/data/articles');
-
-var markdown = require('./utils/markdown');
-
-var blogDir = __dirname;
-
-
 // jade tasks
-
-gulp.task('jade:index', function(callback) {
-    fs.readFile(path.join(blogDir, 'src/templates/index.jade'), 'utf8', function(err, data) {
-        if (err) callback(err);
-        else onReadIndexJadeSuccess(data);
-    });
-
-    function onReadIndexJadeSuccess(data) {
-        var fn = jade.compile(data, {
-            basedir: blogDir,
-            pretty: '    '
-        });
-        var html = fn({articles: articles});
-        fs.writeFile(path.join(blogDir, 'www/index.html'), html, {
-            encoding: 'utf8',
-            mode: 0o644
-        }, callback);
-    }
-});
-
+var renderIndex = require('./utils/render-index');
+gulp.task('jade:index', renderIndex);
 gulp.task('jade', ['jade:index']);
-
-gulp.task('watch:jade', function() {
-    gulp.watch('./src/templates/**/*.jade', ['jade']);
-});
 
 
 // less tasks
-
 var lessSrc = './src/less/**/*.less';
 var cssDest = './www/css/';
 
@@ -71,10 +33,6 @@ gulp.task('less', function() {
     .pipe(gulp.dest(cssDest));
 });
 
-gulp.task('watch:less', function() {
-    gulp.watch(lessSrc, ['less']);
-});
-
 
 // markdown tasks
 gulp.task('markdown', function() {
@@ -86,14 +44,27 @@ gulp.task('markdown', function() {
         .pipe(gulp.dest('./html-articles/'));
 });
 
+// copy tasks
+gulp.task('copy', function() {
+    return gulp.src(['./src/articles/**/*.{jpg,png,gif}'])
+        .pipe(gulp.dest('./www/articles/'));
+});
+
+// watch tasks
+gulp.task('watch:less', function() {
+    gulp.watch(lessSrc, ['less']);
+});
+gulp.task('watch:jade', function() {
+    gulp.watch('./src/templates/**/*.jade', ['jade']);
+});
 gulp.task('watch', ['watch:less', 'watch:jade']);
 
-gulp.task('dev', function(callback) {
-    runSequence('build', 'watch');
-});
 
 // main tasks
 gulp.task('build', function(callback) {
-    runSequence('less', 'jade', callback);
+    runSequence('less', 'jade', 'copy', callback);
+});
+gulp.task('dev', function(callback) {
+    runSequence('build', 'watch');
 });
 gulp.task('default', ['build']);
